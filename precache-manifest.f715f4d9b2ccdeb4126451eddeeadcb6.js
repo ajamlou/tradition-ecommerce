8c4d8ae0a552c1c587c9ed6a2a370f07332e57e4
ajamlou <kevin.ajamlou@gmail.com>
2021-01-05 17:51:29 +0100

    Updates

---
+++ precache-manifest.f715f4d9b2ccdeb4126451eddeeadcb6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd1ecf8e57993c4201f16b8bd2cc089",
+    "url": "/tradition-ecommerce/index.html"
+  },
+  {
+    "revision": "e4e392cf89a7776ceb76",
+    "url": "/tradition-ecommerce/static/css/main.5f78d33b.chunk.css"
+  },
+  {
+    "revision": "e629db75bbe79c2f3e5c",
+    "url": "/tradition-ecommerce/static/js/2.332338f8.chunk.js"
+  },
+  {
+    "revision": "697778febe175c367ce3694268cb4b00",
+    "url": "/tradition-ecommerce/static/js/2.332338f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e392cf89a7776ceb76",
+    "url": "/tradition-ecommerce/static/js/main.e20bfbc0.chunk.js"
+  },
+  {
+    "revision": "15192291bbd00ba57da3",
+    "url": "/tradition-ecommerce/static/js/runtime-main.a1a8dc0c.js"
+  },
+  {
+    "revision": "6bb6138fdbf9465d9ae637169e4b749f",
+    "url": "/tradition-ecommerce/static/media/stripe.6bb6138f.png"
+  },
+  {
+    "revision": "26dfe4fa1020261df058e8b2bc26ba47",
+    "url": "/tradition-ecommerce/static/media/subheader.26dfe4fa.png"
+  },
+  {
+    "revision": "3310e8def13d64491a6e158fd883732d",
+    "url": "/tradition-ecommerce/static/media/wallpaper.3310e8de.jpg"
+  }
+]);
